db4bd2e967103a32f9ff8435a4926623f3666358
Ivan Iacono <ivan.iacono@gmail.com>
2013-08-13 17:34:43 +0100

    updated i2c_demo

--- examples/i2c_demo.erl
+++ examples/i2c_demo.erl
@@ -12,14 +12,17 @@
 -define(IODIR, 16#0).
 -define(GPIO, 16#9).
 
+-define(I2C_CHANNEL, i2c1).
+
 init() ->
-    i2c:start_link(),
+    %% starts the supervisor
+    i2c_sup:start_link([?I2C_CHANNEL]),
     %% initialize i2c bus
-    Fd = i2c:i2c_init(),
+    Fd = i2c:i2c_init(?I2C_CHANNEL),
     %% set pin directions
-    i2c:i2c_write(Fd, ?ADDR, {?IODIR, 16#f}, 2),
+    i2c:i2c_write(?I2C_CHANNEL, Fd, ?ADDR, {?IODIR, 16#f}, 2),
     %% reset pin value
-    i2c:i2c_write(Fd, ?ADDR, {?GPIO, 0}, 2),
+    i2c:i2c_write(?I2C_CHANNEL, Fd, ?ADDR, {?GPIO, 0}, 2),
     Fd.
 
 %% Runs the demo
@@ -31,13 +34,13 @@ run_demo() ->
 %% Main function
 start(Fd, Count) when Count < 16 ->
     %% turn on the led
-    i2c:i2c_write(Fd, ?ADDR, {?GPIO, Count bsl 4}, 2),
+    i2c:i2c_write(?I2C_CHANNEL, Fd, ?ADDR, {?GPIO, Count bsl 4}, 2),
     timer:sleep(1000),
     
     %% read DIP switches
-    i2c:i2c_write(Fd, ?ADDR, {?GPIO}, 1),
+    i2c:i2c_write(?I2C_CHANNEL, Fd, ?ADDR, {?GPIO}, 1),
     timer:sleep(10),
-    {Val}=i2c:i2c_read(Fd, ?ADDR, 1),
+    {Val}=i2c:i2c_read(?I2C_CHANNEL, Fd, ?ADDR, 1),
     io:format("GPIO=~.16B : GP0: ~.16B  GP1: ~.16B  GP2 ~.16B  GP3 ~.16B~n",[Val, Val band 1, (Val band 2) bsr 1, (Val band 4) bsr 2, (Val band 8) bsr 3]),
     start(Fd, Count + 1);
 start(_Fd, _Count) ->
@@ -45,7 +48,8 @@ start(_Fd, _Count) ->
 
 stop(Fd) ->
     %% reset pin value
-    i2c:i2c_write(Fd, ?ADDR, {?GPIO, 0}, 2),
-    i2c:stop(),
+    i2c:i2c_write(?I2C_CHANNEL, Fd, ?ADDR, {?GPIO, 0}, 2),
+    i2c:stop(?I2C_CHANNEL),
+    i2c_sup:stop(),
     bye.
     
