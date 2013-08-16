470a8ffc51576732620177eaf63867eea207fe0e
Ivan Iacono <ivan.iacono@gmail.com>
2013-08-16 15:54:59 +0100

    removed len parameter in the i2c:write calls

--- examples/i2c_demo.erl
+++ examples/i2c_demo.erl
@@ -18,9 +18,9 @@ init() ->
     %% starts the supervisor
     i2c_sup:start_link([{?I2C_CHANNEL, "/dev/i2c-1"}]),
     %% set pin directions
-    i2c:write(?I2C_CHANNEL, ?ADDR, {?IODIR, 16#f}, 2),
+    i2c:write(?I2C_CHANNEL, ?ADDR, {?IODIR, 16#f}),
     %% reset pin value
-    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO, 0}, 2).
+    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO, 0}).
 
 %% Runs the demo
 run_demo() ->
@@ -34,11 +34,11 @@ start() ->
 
 start(Count) when Count < 16 ->
     %% turn on the led
-    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO, Count bsl 4}, 2),
+    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO, Count bsl 4}),
     timer:sleep(1000),
     
     %% read DIP switches
-    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO}, 1),
+    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO}),
     timer:sleep(10),
     {Val}=i2c:read(?I2C_CHANNEL, ?ADDR, 1),
     io:format("GPIO=~.16B : GP0: ~.16B  GP1: ~.16B  GP2 ~.16B  GP3 ~.16B~n",[Val, Val band 1, (Val band 2) bsr 1, (Val band 4) bsr 2, (Val band 8) bsr 3]),
@@ -48,7 +48,7 @@ start(_Count) ->
 
 stop() ->
     %% reset pin value
-    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO, 0}, 2),
+    i2c:write(?I2C_CHANNEL, ?ADDR, {?GPIO, 0}),
     i2c:stop(?I2C_CHANNEL),
     i2c_sup:stop(),
     bye.
